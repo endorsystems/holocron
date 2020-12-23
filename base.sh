2c2ee35be459f3e63239ef8e24e7424ca60527fe
endorsystems <esane.demay@gmail.com>
2020-12-23 12:19:31 -0500

    disabled sshd because not needed

--- base.sh
+++ base.sh
@@ -58,7 +58,7 @@ IFS=$IFS_ORIG
 disk=$(whiptail --backtitle "${APPTITLE}" --title "${1}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
 if [ "$?" != "0" ]
 then
-        return 1
+    return 1
 fi
 
 part_boot="$(ls ${disk%%\ *}* | grep -E "^${disk%%\ *}p?1$")"
@@ -189,7 +189,7 @@ echo ${sudo_user}:${sudo_user_pass} | chpasswd --root /mnt
 # echo "${sudo_user_pass}\n${sudo_user_pass}" | passwd --root /mnt ${sudo_user}
 
 # Systemd enables
-arch-chroot /mnt systemctl enable sshd
+# arch-chroot /mnt systemctl enable sshd
 arch-chroot /mnt systemctl enable avahi-daemon.service
 arch-chroot /mnt systemctl enable cups.service
 arch-chroot /mnt systemctl enable docker
